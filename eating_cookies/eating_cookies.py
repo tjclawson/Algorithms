d5abb8932e2224659bc44e2346e3b0ef1a7f41b6
tjclawson <tjamesclawson@gmail.com>
2020-02-20 11:56:22 -0500

    Add logic to remove values less than n-3 for efficiency

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,14 +7,15 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-    base_case = [1, 1, 2, 4]
+    base_case = [1, 1, 2]
 
-    for i in range(4, n + 1):
-        base_case.append(base_case[i - 1] + base_case[i - 2] + base_case[i - 3])
+    for i in range(3, n):
+        base_case.append(base_case[0] + base_case[1] + base_case[2])
+        base_case.pop(0)
 
-    return base_case[n]
+    return base_case[n] if n < 3 else sum(base_case)
 
-
+print(eating_cookies(1000000))
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
